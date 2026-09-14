8c7748293b0ba92faaa8aa4e34394dbfc1d8a6ec
OleksiiZench <olekzenchenko@gmail.com>
2026-09-14 17:49:26 +0300

    feat: configure ESP32-S3 for USB HID and CDC composite device

--- firmware/LilkaDeck_Firmware/src/main.cpp
+++ firmware/LilkaDeck_Firmware/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#include "USB.h"
+#include "USBHIDKeyboard.h"
 
-// put function declarations here:
-int myFunction(int, int);
+USBHIDKeyboard Keyboard;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // 1. Ініціалізуємо віртуальну клавіатуру (HID)
+    Keyboard.begin();
+    
+    // 2. Запускаємо шину USB
+    USB.begin();
+    
+    // 3. Запускаємо COM-порт. 
+    // Завдяки ARDUINO_USB_CDC_ON_BOOT=1, це піде прямо через USB-кабель.
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Відправляємо тестове повідомлення кожні 2 секунди
+    Serial.println("Lilka Stream Deck is online: COM + HID enabled!");
+    delay(2000);
 }
