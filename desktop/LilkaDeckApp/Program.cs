a9841965328f7af227824b4f35caa7a0f28d0094
OleksiiZench <olekzenchenko@gmail.com>
2026-09-24 18:56:49 +0300

    feat(desktop): prevent multiple application instances via file lock

--- desktop/LilkaDeckApp/Program.cs
+++ desktop/LilkaDeckApp/Program.cs
@@ -1,16 +1,34 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 
 namespace LilkaDeckApp;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
+    private static FileStream? _lockFile;
+
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string lockDir = Path.Combine(appDataPath, "LilkaDeck");
+
+        Directory.CreateDirectory(lockDir);
+        string lockFilePath = Path.Combine(lockDir, "single_instance.lock");
+
+        try
+        {
+            _lockFile = new FileStream(lockFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("LilkaDeck вже запущено. Закриваємо дублікат.");
+            return;
+        }
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
